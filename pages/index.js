9c9b29418b3183549a78a23bd0c3df71a4176259
GeForceGroot <tiwarishreyansh007@gmail.com>
2023-07-09 16:33:07 +0530

    welcome 1

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-
+     <Head>
+      <title>Kshianki: Discover Bedding Bliss for Every Night!</title>
+      <meta name='description' content='Kshianki: Discover Bedding Bliss for Every Night!'></meta>
+      <link rel='icon' href='/favicon.ico'></link>
+     </Head>
     </>
   )
 }
