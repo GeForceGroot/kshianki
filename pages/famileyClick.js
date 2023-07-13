86bc961d03f86a8e79e5a98ef5267dbb938a523e
GeForceGroot <tiwarishreyansh007@gmail.com>
2023-07-13 16:11:28 +0530

    welcome 10

--- pages/famileyClick.js
+++ pages/famileyClick.js
@@ -3,42 +3,42 @@ import React from 'react'
 const FamileyClick = () => {
     return (
         <>
-            <div className='text-4xl font-semibold justify-normal flex-auto mt-20 ml-10'>🖤 Turkish Style 100% Pure Cotton Towles</div>
-            <section class="text-gray-600 body-font">
-                <div class="container px-5 py-24 mx-auto">
-                    <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
-                        <div class="p-4 md:w-1/3 sm:mb-0 mb-6 ">
-                            <div class="rounded-lg h-64 overflow-hidden border border-gray-400">
-                                <img alt="content" class="object-cover object-center h-full w-full" src="/div.jpg"/>
+            <div className='text-4xl font-semibold justify-normal flex-auto mt-20 ml-10'>🖤 "KSHIANKI® Familey Clicks"</div>
+            <section className="text-gray-600 body-font">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
+                        <div className="p-4 md:w-1/3 sm:mb-0 mb-6 ">
+                            <div className="rounded-lg h-64 overflow-hidden border border-gray-400">
+                                <img alt="content" className="object-cover object-center h-full w-full" src="/red.jpg"/>
                             </div>
-                            <h2 class="text-xl font-medium title-font text-gray-900 mt-5">Diwan Set</h2>
-                            <p class="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
-                            <a class="text-pink-700 inline-flex items-center mt-3 font-bold text-lg">Buy Diwan Set
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                            <h2 className="text-2xl font-medium title-font mt-5 text-red-600">Single Bedsheets</h2>
+                            <p className="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
+                            <a className="text-red-600 inline-flex items-center mt-3 font-bold text-lg">Explore More
+                                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                                 </svg>
                             </a>
                         </div>
-                        <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
-                            <div class="rounded-lg h-64 overflow-hidden">
-                                <img alt="content" class="object-cover object-center h-full w-full" src="https://dummyimage.com/1204x504"/>
+                        <div className="p-4 md:w-1/3 sm:mb-0 mb-6 ">
+                            <div className="rounded-lg h-64 overflow-hidden border border-gray-400">
+                                <img alt="content" className="object-cover object-center h-full w-full" src="/orange.jpg"/>
                             </div>
-                            <h2 class="text-xl font-medium title-font text-gray-900 mt-5">The Catalyzer</h2>
-                            <p class="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
-                            <a class="text-indigo-500 inline-flex items-center mt-3">Learn More
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                            <h2 className="text-2xl font-medium title-font text-orange-500 mt-5">Pure Cotton Bedsheet</h2>
+                            <p className="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
+                            <a className="text-orange-500 inline-flex items-center mt-3 font-bold text-lg">Explore More
+                                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                                 </svg>
                             </a>
                         </div>
-                        <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
-                            <div class="rounded-lg h-64 overflow-hidden">
-                                <img alt="content" class="object-cover object-center h-full w-full" src="https://dummyimage.com/1205x505"/>
+                        <div className="p-4 md:w-1/3 sm:mb-0 mb-6 ">
+                            <div className="rounded-lg h-64 overflow-hidden border border-gray-400">
+                                <img alt="content" className="object-cover object-center h-full w-full" src="/yellow.jpg"/>
                             </div>
-                            <h2 class="text-xl font-medium title-font text-gray-900 mt-5">The 400 Blows</h2>
-                            <p class="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
-                            <a class="text-indigo-500 inline-flex items-center mt-3">Learn More
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                            <h2 className="text-2xl font-medium title-font text-yellow-400 mt-5">Kid's Bedsheets</h2>
+                            <p className="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
+                            <a className="text-yellow-400 inline-flex items-center mt-3 font-bold text-lg">Explore More
+                                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                                 </svg>
                             </a>
@@ -46,41 +46,41 @@ const FamileyClick = () => {
                     </div>
                 </div>
             </section>
-            <section class="text-gray-600 body-font">
-                <div class="container px-5 py-24 mx-auto">
-                    <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
-                        <div class="p-4 md:w-1/3 sm:mb-0 mb-6 ">
-                            <div class="rounded-lg h-64 overflow-hidden border border-gray-400">
-                                <img alt="content" class="object-cover object-center h-full w-full" src="/div.jpg"/>
+            <section className="text-gray-600 body-font">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
+                        <div className="p-4 md:w-1/3 sm:mb-0 mb-6 ">
+                            <div className="rounded-lg h-64 overflow-hidden border border-gray-400">
+                                <img alt="content" className="object-cover object-center h-full w-full" src="/green.jpg"/>
                             </div>
-                            <h2 class="text-xl font-medium title-font text-gray-900 mt-5">Diwan Set</h2>
-                            <p class="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
-                            <a class="text-pink-700 inline-flex items-center mt-3 font-bold text-lg">Buy Diwan Set
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                            <h2 className="text-2xl font-medium title-font text-green-600 mt-5">Cushion Cover</h2>
+                            <p className="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
+                            <a className="text-green-700 inline-flex items-center mt-3 font-bold text-lg">Buy Diwan Set
+                                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                                 </svg>
                             </a>
                         </div>
-                        <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
-                            <div class="rounded-lg h-64 overflow-hidden">
-                                <img alt="content" class="object-cover object-center h-full w-full" src="https://dummyimage.com/1204x504"/>
+                        <div className="p-4 md:w-1/3 sm:mb-0 mb-6 ">
+                            <div className="rounded-lg h-64 overflow-hidden border border-gray-400">
+                                <img alt="content" className="object-cover object-center h-full w-full" src="/blue.jpg"/>
                             </div>
-                            <h2 class="text-xl font-medium title-font text-gray-900 mt-5">The Catalyzer</h2>
-                            <p class="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
-                            <a class="text-indigo-500 inline-flex items-center mt-3">Learn More
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                            <h2 className="text-2xl font-medium title-font text-blue-500 mt-5">Table Cover</h2>
+                            <p className="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
+                            <a className="text-blue-700 inline-flex items-center mt-3 font-bold text-lg">Buy Diwan Set
+                                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                                 </svg>
                             </a>
                         </div>
-                        <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
-                            <div class="rounded-lg h-64 overflow-hidden">
-                                <img alt="content" class="object-cover object-center h-full w-full" src="https://dummyimage.com/1205x505"/>
+                        <div className="p-4 md:w-1/3 sm:mb-0 mb-6 ">
+                            <div className="rounded-lg h-64 overflow-hidden border border-gray-400">
+                                <img alt="content" className="object-cover object-center h-full w-full" src="/violet.jpg"/>
                             </div>
-                            <h2 class="text-xl font-medium title-font text-gray-900 mt-5">The 400 Blows</h2>
-                            <p class="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
-                            <a class="text-indigo-500 inline-flex items-center mt-3">Learn More
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                            <h2 className="text-2xl font-medium title-font text-purple-600 mt-5">Diwan Set</h2>
+                            <p className="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
+                            <a className="text-purple-600 inline-flex items-center mt-3 font-bold text-lg">Buy Diwan Set
+                                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                                 </svg>
                             </a>
