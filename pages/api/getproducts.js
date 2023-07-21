49670abeb1916b02fbcdb1f7ab4be97a7b6b6151
GeForceGroot <tiwarishreyansh007@gmail.com>
2023-07-21 22:59:40 +0530

    welcome 23

--- pages/api/getproducts.js
+++ pages/api/getproducts.js
@@ -2,9 +2,31 @@
 import Product from "@/models/Product"
 import connectDb from "@/middleware/mongoose"
 
-const handler = async (req, res)=>{
+const handler = async (req, res) => {
     let products = await Product.find()
-    res.status(200).json({ products })
+
+    let singleBedSheets = {}
+
+    for (let item of products) {
+
+        if (item.title in singleBedSheets) {
+
+            if (!singleBedSheets[item.title].color.includes(item.color) && item.availableQty > 0) {
+                singleBedSheets[item.title].color.push(item.color)
+            }
+
+        }
+        else {
+            singleBedSheets[item.title] = JSON.parse(JSON.stringify(item))
+            if (item.availableQty > 0) {
+                singleBedSheets[item.title].color = [item.color]
+            }
+        }
+
+    }
+
+
+    res.status(200).json({ singleBedSheets })
 }
 
 export default connectDb(handler);
